5c096121e9b1de1a4c1c9db41efcc60128ca73f9
downleave <downleave@qq.com>
2018-12-24 17:00:39 +0800

    加入了cookied读写

--- wikicrawler.py
+++ wikicrawler.py
@@ -4,99 +4,69 @@ from PIL import Image
 from selenium import webdriver
 import os
 import time
+import json
 
 def login(browser):
 	loginPage = 'http://wiki.klub11.com/index.php?s=/home/item/pwd/item_id/2'
 	browser.get(loginPage)
-	# session = requests.session()
-	# response = session.get(loginPage)
-	# path = downloadCaptcha(response,session)
-	# showCaptcha(path)
 	v_code = input('请输入验证码:')
 	return submitLoginForm(v_code,browser)
 
-# def downloadCaptcha(response,session):
-# 	imgUrl = 'http://wiki.klub11.com/Public/verifyCode.php';
-# 	captha = session.get(imgUrl)
-# 	path = 'captha.jpg'
-# 	with open(path, 'wb') as file:
-# 		file.write(captha.content)
-# 		file.close()
-# 	return path
-
-# def showCaptcha(path):
-# 	im = Image.open(path)
-# 	im.show()
-
 def submitLoginForm(v_code, browser):
-	# formdata = {
-	# 	"item_id": "2",
-	# 	"password": "pf1q78rc",
-	# 	"refer_url": "",
-	# 	"v_code": v_code,
-	# }
-	# headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0"}
-	# response = session.post('http://wiki.klub11.com/index.php?s=/home/item/pwd/item_id/2', headers = headers, data = formdata)
 	browser.find_element_by_xpath('//input[@name="password"]').send_keys("pf1q78rc")
 	browser.find_element_by_xpath('//input[@name="v_code"]').send_keys(v_code)
 	browser.find_element_by_xpath('//button[@class="golddot_btn"]').click()
-	# if response.status_code == 200:
-	# 	print('登录成功')
-	# else:
-	# 	print('登录失败')
 	return browser
 
-# def writeFollowers(session, nextpage = ''):
-# 	response 	= session.get('https://www.douban.com/contacts/rlist' + nextpage)
-# 	response 	= etree.HTML(response.text)
-# 	followerList 	= response.xpath('//ul[@class="user-list"]/li/div/h3/a[1]/text()')
-# 	detailUrlList = response.xpath('//ul[@class="user-list"]/li/div/h3/a[1]/@href')
+def writeJson(browser, nextpage='2'):
+	browser.get('http://wiki.klub11.com/index.php?s=/2&page_id=' + nextpage)
+	time.sleep(2)
 
-# 	file = 'follow.txt'
+	#切换iframe
+	browser.switch_to.frame("page-content")
 
-# 	if not os.path.exists(file):
-#  		fileobj = os.open(file, os.O_CREAT|os.O_RDWR|os.O_APPEND)
-# 	else:
-# 		fileobj = os.open(file, os.O_RDWR|os.O_APPEND)
+	#获取多个接口url
+	urls = browser.find_elements_by_xpath("//p/strong[text()='请求URL：']/../following-sibling::ul[1]/li/code")
+	for url in urls:
+		print('url:',url.text)
 
-# 	for index,url in enumerate(detailUrlList):
-# 		followerDetailResponse = etree.HTML(session.get(url).text)
-# 		location = followerDetailResponse.xpath('//div[@class="basic-info"]/div/a/text()')
-# 		if not location:
-# 			location = ''
-# 		else:
-# 			location = location[0]
-# 		writeStr = followerList[index] + ' ' + location + '\r\n'
-# 		os.write(fileobj, bytes(writeStr, 'UTF-8'))
+	#获取请求方式
+	apitypes = browser.find_elements_by_xpath("//p/strong[text()='请求方式：']/../following-sibling::ul[1]/li")
+	for apitype in apitypes:
+		print('apitype:',apitype.text)
 
-# 	print('分页信息写入成功')
+	params = browser.find_elements_by_xpath("//ul/li[text()='content参数说明']/../following-sibling::div[1]/table/tbody/tr/td[1]")
 
-# 	nextpage = response.xpath('//span[@class="next"]/a/@href')
-# 	if nextpage:
-# 		writeFollowers(session, nextpage[0])
+	for param in params:
+		print('param:',param.text)
 
-def writeJson(browser, nextpage='4'):
-	# response = session.get('http://wiki.klub11.com/index.php?s=/2&page_id=' + nextpage)
-	browser.get('http://wiki.klub11.com/index.php?s=/home/page/index/page_id/' + nextpage)
-	page_title = browser.find_element_by_xpath('//h3[@id="page_title"]').text
-	# response = etree.HTML(response.text)
-	# 接口中文名字
-	# page_title = response.xpath('//h3[@id="page_title"]/text()')[0]
-	print(page_title)
-	url = browser.find_element_by_xpath('//ul/li/code').text
-	print(url)
-
-	# urls = response.xpath('//li/code')
-	# print(urls)
-	exit()
+	browser.quit()
 
 def browserInit():
 	chromedriver = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe"
 	os.environ["webdriver.chrome.driver"] = chromedriver
 	return webdriver.Chrome(chromedriver)
 
+def writeCookie(browser):
+	cookie = browser.get_cookies()
+	cookie = json.dumps(cookie)
+	path = 'cookie.json'
+	fo = open(path, "w")
+	fo.write(cookie)
+	fo.close()
+
+def readCookie(browser):
+	#要先访问一下才能add_cookie
+	loginPage = 'http://wiki.klub11.com/index.php?s=/home/item/pwd/item_id/2'
+	browser.get(loginPage)
+	fo = open('cookie.json', 'r')
+	cookie = fo.read()
+	cookie = json.loads(cookie)
+	browser.add_cookie(cookie[0])
+	return True;
 
 browser = browserInit()
-browser = login(browser)
+if readCookie(browser) == False:
+	browser = login(browser)
+	writeCookie(browser)
 writeJson(browser)
-#writeFollowers(session)

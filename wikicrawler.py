2e1d7204cd6cc30a75f83a610e4105ccfa102f92
downleave <downleave@qq.com>
2018-12-25 18:48:59 +0800

    整理代码，方便修改

--- wikicrawler.py
+++ wikicrawler.py
@@ -18,17 +18,26 @@ def submitLoginForm(v_code, browser):
 	browser.find_element_by_xpath('//button[@class="golddot_btn"]').click()
 	return browser
 
-def writeJson(browser, nextpage='2'):
-	browser.get('http://wiki.klub11.com/index.php?s=/2&page_id=' + nextpage)
-	# time.sleep(2)
-
-	#切换iframe
-	browser.switch_to.frame("page-content")
-
+def getUrls(browser):
 	#获取多个接口url
 	urls = browser.find_elements_by_xpath("//p/strong[text()='请求URL：']/../following-sibling::ul[1]/li/code")
+	if len(urls) == 0:
+		urls = browser.find_elements_by_xpath("//strong[text()='请求URL：']/following-sibling::ul[1]/li/code")
+	return urls
+
+def  getTypes(browser):
 	apitypes = browser.find_elements_by_xpath("//p/strong[text()='请求方式：']/../following-sibling::ul[1]/li")
-		
+	return apitypes
+
+def getParams(browser,num):
+	params = {}
+	params['name'] = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[1]"%(num))
+	params['require'] = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[2]"%(num))
+	params['type'] = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[3]"%(num))
+	params['description'] = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[4]"%(num))
+	return params
+
+def getList(browser, urls, apitypes):
 	apiList = []
 	for index,url in enumerate(urls):
 		tmp = {}
@@ -38,31 +47,29 @@ def writeJson(browser, nextpage='2'):
 
 		num = index + 1
 
-		params_name = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[1]"%(num))
-		params_require = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[2]"%(num))
-		params_type = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[3]"%(num))
-		params_description = browser.find_elements_by_xpath("(//p/strong[text()='请求方式：'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[4]"%(num))
+		
+		params = getParams(browser,num)
 
 		i = 1;
-		for key,param_name in enumerate(params_name):
+		for key,param_name in enumerate(params['name']):
 			#遍历父级
 			tmp_param = {}
 			tmp_param['id'] = i
 			tmp_param['pid'] = 0
-			tmp_param['fieldname'] = params_name[key].text
-			tmp_param['require'] = params_require[key].text
-			tmp_param['type'] = params_type[key].text		
-			tmp_param['description'] = params_description[key].text
+			tmp_param['fieldname'] = params['name'][key].text
+			tmp_param['require'] = params['require'][key].text
+			tmp_param['type'] = params['type'][key].text		
+			tmp_param['description'] = params['description'][key].text
 			tmp['params'].append(tmp_param)
 			
 			#如果有json，则遍历json相关参数
 			if(tmp_param['type'] == 'json'):
 				pid = i
 				i = i + 1
-				content_params_name = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[1]"%(params_name[key].text,num))
-				content_params_require = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[2]"%(params_name[key].text,num))
-				content_params_type = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[3]"%(params_name[key].text,num))
-				content_params_description = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[4]"%(params_name[key].text,num))
+				content_params_name = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[1]"%(params['name'][key].text,num))
+				content_params_require = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[2]"%(params['name'][key].text,num))
+				content_params_type = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[3]"%(params['name'][key].text,num))
+				content_params_description = browser.find_elements_by_xpath("(//ul/li[text()='%s参数说明'])[%s]/../following-sibling::div[1]/table/tbody/tr/td[4]"%(params['name'][key].text,num))
 				for key2,content_param_name in enumerate(content_params_name):
 					tmp_param = {}
 					tmp_param['id'] = i
@@ -76,6 +83,9 @@ def writeJson(browser, nextpage='2'):
 			else:
 				i = i + 1
 		apiList.append(tmp)
+	return apiList
+
+def writeJson(apiList,nextpage):
 	if len(apiList):
 		apiList = json.dumps(apiList)
 		path = '%s.json'%(nextpage)
@@ -83,7 +93,23 @@ def writeJson(browser, nextpage='2'):
 		fo.write(apiList)
 		fo.close()
 
-	# browser.quit()
+def crawlWiki(browser, nextpage='2'):
+	browser.get('http://wiki.klub11.com/index.php?s=/2&page_id=' + nextpage)
+
+	#切换iframe
+	browser.switch_to.frame("page-content")
+
+	#获取页面所有请求url
+	urls = getUrls(browser)
+
+	#获取页面所有请求方式
+	apitypes = getTypes(browser)
+		
+	#根据url和apitypes来再去爬出对应的参数，并重组成dict
+	apiList = getList(browser, urls, apitypes)
+	
+	#将apiList转成json并写入文件
+	writeJson(apiList,nextpage)
 
 def browserInit():
 	chromedriver = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe"
@@ -106,13 +132,20 @@ def readCookie(browser):
 	cookie = fo.read()
 	cookie = json.loads(cookie)
 	browser.add_cookie(cookie[0])
-	return True;
+	# 尝试是否登录成功
+	browser.get('http://wiki.klub11.com/index.php?s=/2&page_id=2')
+	browser.switch_to.frame("page-content")
+	check_is_login = browser.find_elements_by_xpath("//h3[text()='会员资料创建和变动']")
+	if len(check_is_login):
+		return True
+	return False
 
 browser = browserInit()
 if readCookie(browser) == False:
 	browser = login(browser)
 	writeCookie(browser)
 # for i in range(2,20):
-# 	writeJson(browser,str(i))
+# 	crawlWiki(browser,str(i))
 # 	time.sleep(2)
-writeJson(browser,'96')
+crawlWiki(browser,'73')
+browser.quit()
